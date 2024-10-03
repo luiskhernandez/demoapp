d06a9285863d7ccb210c2ec8ab6c9837b0df63bf
Luis Carlos Hernandez <luis.macias@koombea.com>
2024-10-03 06:21:57 -0500

    Add render config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+    config.view_component.show_previews = true
+    config.view_component.show_previews_source = true
 end
